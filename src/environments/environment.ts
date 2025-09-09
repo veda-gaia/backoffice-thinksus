106ab3844c6cb7a2659d4135434c457e47bb5f90
bruno-bogochvol <bruno@codefabrik.dev>
2025-09-08 21:59:57 -0300

    update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
 
   api: {
-    // path: 'https://thinksus-api.com',
-    path: "http://localhost:6001",
-    // path: 'https://hom.thinksus-api.com',
+    // path: "https://thinksus-api.com",
+    // path: "http://localhost:6001",
+    path: "https://hom.thinksus-api.com",
   },
 
   encrypt_key: "5e99d833-bcfa-411e-ad92-97686ca6890c",
